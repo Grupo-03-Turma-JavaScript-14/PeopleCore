2e6cdc03fd2c9117500cdc2030a7cf4410c1a83c
Bianca <bianca.alves.0704@gmail.com>
2026-05-22 14:44:16 -0300

    autorizacao de origin em enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173/',
+      'http://localhost:5174/',
+      'https://peoplecore.gbworks.com.br/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('PeopleCore')
     .setDescription('API backend para sistema de RH com autenticação JWT')
